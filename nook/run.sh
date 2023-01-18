10cfac28ba852a63295e4a10da361d592ec94d20
Glenn Hope <glenn.alexander.hope@gmail.com>
2023-01-18 00:51:34 -0800

    Make work on OpenBSD

--- nook/run.sh
+++ nook/run.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mkdir -p /dev/shm/public
-(cd /dev/shm/public && php -S 0.0.0.0:8000) &
+tmpdir="/mem/personalsign"
+php_cmd="php-8.1"
+
+mkdir -p "${tmpdir}"
+(cd "${tmpdir}" && ${php_cmd} -S 0.0.0.0:8000) &
 while :; do
-	php nook.php /dev/shm/public/index.html
+	${php_cmd} nook.php "${tmpdir}"/index.html
 	#wait
 	echo "Encountered error, Ctrl-C to exit, otherwise will try again."
 	sleep 60

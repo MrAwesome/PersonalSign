2ae24314a971ef7e5d3b16189187030fc85ea8f7
Glenn Hope <glenn.alexander.hope@gmail.com>
2023-01-26 15:44:17 -0800

    Update run script

--- backend/run.sh
+++ backend/run.sh
@@ -4,19 +4,19 @@ cd "$(dirname "$0")"
 
 yarn
 
-if [ -d "/mem" ]; then
-    tmpdir="/mem/personalsign"
-else
-    tmpdir="/tmp/personalsign"
-fi
+#if [ -d "/mem" ]; then
+    #tmpdir="/mem/personalsign"
+#else
+    #tmpdir="/tmp/personalsign"
+#fi
 
 # Run a simple web server
-mkdir -p "${tmpdir}"
-(cd "${tmpdir}" && php -S 0.0.0.0:8000) &
+#mkdir -p "${tmpdir}"
+#(cd "${tmpdir}" && php -S 0.0.0.0:8000) &
 
 # Generate the index.html file that web server will serve
-while :; do
-    yarn run ts-node src/index.ts
-    sleep 600
-    git pull --rebase --autostash
-done
+#while :; do
+yarn run ts-node src/index.ts
+    #sleep 600
+    #git pull --rebase --autostash
+#done

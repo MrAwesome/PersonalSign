501db53410e680987f1c3c7c024e8ea2e806727e
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-02-28 15:23:19 +0100

    alias yarn

--- backend/run.sh
+++ backend/run.sh
@@ -2,17 +2,18 @@
 
 cd "$(dirname "$0")" || exit 1
 
+yarncmd=yarn
 if command -v yarnpkg &>/dev/null; then
-    alias yarn=yarnpkg
+    yarncmd=yarnpkg
 fi
 
-yarn || exit 1
+$yarncmd || exit 1
 
 while :; do
     git fetch origin main
     git reset --hard origin/main
 
-    yarn run ts-node src/index.ts
+    $yarncmd run ts-node src/index.ts
 
     echo "Server was shut down, restarting..."
     sleep 10

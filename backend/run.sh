8eb0a14827da11f4fa05406aadbc17eb9a3ca008
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-02-28 10:49:17 +0100

    Update run script

--- backend/run.sh
+++ backend/run.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 
-yarn
+yarn || exit 1
 
-#if [ -d "/mem" ]; then
-    #tmpdir="/mem/personalsign"
-#else
-    #tmpdir="/tmp/personalsign"
-#fi
-
-# Run a simple web server
-#mkdir -p "${tmpdir}"
-#(cd "${tmpdir}" && php -S 0.0.0.0:8000) &
-
-# Generate the index.html file that web server will serve
 while :; do
+    git pull --rebase --autostash
+
     yarn run ts-node src/index.ts
-    ## Restart the web server in case of a crash, hopefully this is not necessary:
+
     echo "Server was shut down, restarting..."
     sleep 10
-    #git pull --rebase --autostash
 done
